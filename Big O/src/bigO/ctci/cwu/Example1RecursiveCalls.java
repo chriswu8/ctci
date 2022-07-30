dc7833c3464187f5c8a4fad3287817bab4148597
Chris Wu <zezemint@gmail.com>
2022-07-30 14:44:33 -0700

    example1

---
+++ Big O/src/bigO/ctci/cwu/Example1RecursiveCalls.java
@@ -0,0 +1,16 @@
+package bigO.ctci.cwu;
+
+/**
+ * Example1RecursiveCalls represents the first example on page 40 of Cracking the Coding Interview book.
+ */
+public class Example1RecursiveCalls
+{
+    /**
+     * 
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+
+    }
+}
